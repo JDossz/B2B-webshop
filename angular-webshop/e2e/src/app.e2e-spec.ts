ddc4de69f17091fde70770d68ef176a02d307edf
Oláh Dániel <olah.daniel95@gmail.com>
2019-09-18 15:38:05 +0200

    changed ng version

--- angular-webshop/e2e/src/app.e2e-spec.ts
+++ angular-webshop/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-webshop!');
+    expect(page.getTitleText()).toEqual('angular-webshop app is running!');
   });
 
   afterEach(async () => {
