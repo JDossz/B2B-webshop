81d56d675117b14a9db6c2ac9135a117989a3f38
Szilvia Kapuvári <kapuvari.szilvia@gmail.com>
2019-09-19 18:23:49 +0200

    orders CRUD added

--- angular-webshop/src/app/app-routing.module.ts
+++ angular-webshop/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrdersComponent } from './page/orders/orders.component';
+import { UpdateOrdersComponent } from './page/update-orders/update-orders.component';
+import { CreateOrdersComponent } from './page/create-orders/create-orders.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OrdersComponent
+  },
+  {
+    path: 'orders/:id',
+    component: UpdateOrdersComponent
+  },
+  {
+    path: 'create-order',
+    component: CreateOrdersComponent
+  },
+  {
+    path: '**',
+    component: OrdersComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-webshop/src/app/app.module.ts
+++ angular-webshop/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './page/orders/orders.component';
+import { UpdateOrdersComponent } from './page/update-orders/update-orders.component';
+import { CreateOrdersComponent } from './page/create-orders/create-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    OrdersComponent,
+    UpdateOrdersComponent,
+    CreateOrdersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
