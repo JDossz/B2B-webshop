9e012bf8bca83c738e0c14ccee90c5cc4473a57f
Pál Sára <pal.sara.ajandek@gmail.com>
2019-09-20 17:42:24 +0200

    sidebar added, style changes, search function missing

--- express-webshop/routes/projects.js
+++ express-webshop/routes/projects.js
@@ -10,6 +10,36 @@ router.get('/', async (req, res, next) => {
     projects: await projectsDB.read(),
   });
 });
+router.get('/arts', async (req, res, next) => {
+  const projects = await projectsDB.read();  
+  res.render('projects', {
+    projects: projects.filter(project => project.category === 'arts'),
+  });
+});
+router.get('/biology', async (req, res, next) => {
+  const projects = await projectsDB.read();  
+  res.render('projects', {
+    projects: projects.filter(project => project.category === 'biology'),
+  });
+});
+router.get('/gender', async (req, res, next) => {
+  const projects = await projectsDB.read();  
+  res.render('projects', {
+    projects: projects.filter(project => project.category === 'gender'),
+  });
+});
+router.get('/technology', async (req, res, next) => {
+  const projects = await projectsDB.read();  
+  res.render('projects', {
+    projects: projects.filter(project => project.category === 'technology'),
+  });
+});
+router.get('/neurology', async (req, res, next) => {
+  const projects = await projectsDB.read();  
+  res.render('projects', {
+    projects: projects.filter(project => project.category === 'neurology'),
+  });
+});
 router.get('/:id', async (req, res, next) => {
   const selectedProject = await projectsDB.read(req.params.id);
   res.render('projectDetails', {
