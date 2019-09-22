f49865a7f2b67682f533ad27b28ae1d3183965ed
Oláh Dániel <olah.daniel95@gmail.com>
2019-09-23 00:20:12 +0200

    Edit: Cleaned up unneccessary angular testing components and services

--- angular-webshop/src/app/app-routing.module.ts
+++ angular-webshop/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TestComponent } from './components/test/test.component';
 
-const routes: Routes = [
-  {
-    path: '**',
-    component: TestComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
