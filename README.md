9d4d045e34fcda9b7df0861a4523ad0dd07b55da
Juhasz Dorottya <juhasz.dossz@gmail.com>
2019-09-23 17:30:42 +0200

    Edit: minor changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # B2B-webshop
+
+# Ha az Express indulásakor hibát kapsz, írd át a mariadb-main.js-ben a jelszót a saját HeidiSQL-es jelszavadra!
+
--- express-webshop/modules/webshop-mariadb/mariadb-main.js
+++ express-webshop/modules/webshop-mariadb/mariadb-main.js
@@ -1,16 +1,20 @@
 const mariadb = require('mariadb');
+
 const pool = mariadb.createPool({
   database: 'betag',
   user: 'root',
-  password: 'ROOT',
+  password: 'root',
   connectionLimit: 5,
 });
 
 const WhereGenerator = require('./tools/where-generator');
+
 const whereGenerator = new WhereGenerator();
 const ListGenerator = require('./tools/list-generator');
+
 const listGenerator = new ListGenerator();
 const SetGenerator = require('./tools/set-generator');
+
 const setGenerator = new SetGenerator();
 
 module.exports = class BetagDB {
@@ -26,16 +30,16 @@ module.exports = class BetagDB {
    * @returns The result of your post query.
    */
   async createRecord(tableName, data) {
-    let query = `INSERT INTO ${tableName} (${listGenerator.getFieldNames(data)}) 
+    const query = `INSERT INTO ${tableName} (${listGenerator.getFieldNames(data)}) 
                  VALUES (${listGenerator.getFieldValues(data)})`;
     return await this.connection.query(query.concat(';'));
   }
 
   /**
    * Concats the query and reads the MySQL database table accordingly.
-   * @param {string} tableName The MySQL table, you want to read from. 
+   * @param {string} tableName The MySQL table, you want to read from.
    * @param {req.query} queryObject The request URL query string object.
-   * @returns The read data from your MySQL database. 
+   * @returns The read data from your MySQL database.
    */
   async readRecord(tableName, queryObject) {
     let query = `SELECT * FROM ${tableName}`;
@@ -48,7 +52,7 @@ module.exports = class BetagDB {
    * @param {*} tableName The MySQL table, where you want to change a record
    * @param {req.query} queryObject The URL query string object.
    * @param {req.body} data The data to be changed in your table.
-   * @returns The result of your update query 
+   * @returns The result of your update query
    */
   async updateRecord(tableName, queryObject, data) {
     let query = `UPDATE ${tableName} SET ${setGenerator.getSetString(data)}`;
@@ -75,4 +79,4 @@ module.exports = class BetagDB {
     return await this.connection.query(query.concat(';'));
   }
 
-}
+};
