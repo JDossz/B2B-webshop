19599ee129583d90bc47a555fa67b0cbf911ae65
Procisz <agostonzajkas@gmail.com>
2019-09-23 10:15:18 +0200

    .

--- angular-webshop/src/app/app-routing.module.ts
+++ angular-webshop/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '*', component: AppComponent },
+  { path: '/statistics', component: StatisticsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-webshop/src/app/app.module.ts
+++ angular-webshop/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StatisticsComponent,
   ],
   imports: [
     BrowserModule,
