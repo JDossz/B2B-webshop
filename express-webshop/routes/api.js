23ac5e2950f81141d0a5560cc2fab5b97b12e44b
Oláh Dániel <olah.daniel95@gmail.com>
2019-09-23 09:33:43 +0200

    Feat: Basic database module done

--- express-webshop/routes/api.js
+++ express-webshop/routes/api.js
@@ -5,7 +5,7 @@ const Database = require('./../modules/webshop-mariadb');
 const database = new Database();
 
 /**
- * Executes POST requests at http://localhost
+ * Executes POST requests at http://localhost:3000/tablename
  */
 router.post('/:table', async (req, res) => {
   res.json(req.body);
@@ -20,7 +20,16 @@ router.get('/:table', async (req, res) => {
 });
 
 /**
- * 
+ * Gets a specific product from the database, based on seo property.
+ */
+router.get('/:table/:seo', async (req, res) => {
+  res.json(await database.readRecord(req.params.table, {
+    "seo": req.params.seo,
+  }))
+});
+
+/**
+ * Executes PUT requests at http://localhost:3000/tablename
  */
 router.put('/:table', async (req, res) => {
   res.json(await database.updateRecord(req.params.table, req.query, req.body));
