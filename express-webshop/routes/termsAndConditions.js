54b343c742151534eaf7543313f68c550aa210e4
Juhasz Dorottya <juhasz.dossz@gmail.com>
2019-09-25 11:02:32 +0200

    Feat: links added, but get 404 instead of rendered page

--- express-webshop/routes/termsAndConditions.js
+++ express-webshop/routes/termsAndConditions.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET Terms page. */
-router.get('/', (req, res, next) => {
+router.get('/terms', (req, res, next) => {
   res.render('termsAndConditions', { title: 'Terms and Conditions' });
 });
 
