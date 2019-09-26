1dc002f329f791bc1cca1f6d319bb518e9365c63
Juhasz Dorottya <juhasz.dossz@gmail.com>
2019-09-26 11:37:30 +0200

    Feat: login redesigned

--- express-webshop/routes/contact.js
+++ express-webshop/routes/contact.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  res.render('about', {
+  res.render('contact', {
     title: 'Contacts',
     user: req.user || {},
   });
