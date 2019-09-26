5ed25573bc2643061663bd81a4cb08b7c2dd77f5
Procisz <agostonzajkas@gmail.com>
2019-09-26 11:52:21 +0200

    .

--- angular-webshop/src/app/page/statistics-basic/statistics-basic.component.ts
+++ angular-webshop/src/app/page/statistics-basic/statistics-basic.component.ts
@@ -10,12 +10,33 @@ import { Observable } from 'rxjs';
 export class StatisticsBasicComponent implements OnInit {
 
   constructor(private dataService: DataService) { }
+
+  /*Variables*/
   orderList: any
   orderList$: Observable<any> = this.dataService.readTableByQuery('orders', {});
+  userList: any
+  userList$: Observable<any> = this.dataService.readTableByQuery('users', {});
+  projectList: any
+  projectList$: Observable<any> = this.dataService.readTableByQuery('projects', {});
 
   ngOnInit() {
+    /*Orders*/
     this.dataService.readTableByQuery('orders', {}).subscribe(orders => {
       this.orderList = orders;
+      console.log(this.orderList);
+      err => console.error(err)
+    })
+
+    /*Users*/
+    this.dataService.readTableByQuery('orders', {}).subscribe(users => {
+      this.userList = users;
+      console.log(this.userList);
+      err => console.error(err)
+    })
+
+    /*Projects*/
+    this.dataService.readTableByQuery('projects', {}).subscribe(projects => {
+      this.orderList = projects;
       console.log(this.orderList);
       err => console.error(err)
     })
