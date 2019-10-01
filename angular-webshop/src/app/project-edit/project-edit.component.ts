3f4023a8fc96cf4f5ce3c0a3805b8c796bcce465
Oláh Dániel <olah.daniel95@gmail.com>
2019-10-01 10:24:15 +0200

    Unstable: project-edit rewrite

--- angular-webshop/src/app/project-edit/project-edit.component.ts
+++ angular-webshop/src/app/project-edit/project-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../model/project';
-import { ProjectService } from '../service/project.service';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { DataService } from '../services/data.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-project-edit',
@@ -11,25 +11,26 @@ import { DataService } from '../services/data.service';
 })
 export class ProjectEditComponent implements OnInit {
 
-  project: Project;
+  project: Project = this.ds.projectList.getValue();
 
   ngOnInit() {
   }
+
   constructor(private ds: DataService, private ar: ActivatedRoute, private router: Router) {
     this.ar.params.forEach(params => {
-      this.ds.readTableBySeoName('projects', params.seo).subscribe(
-        project => this.project = project
-      )
+      this.ds.readTableByQuery('projects', {
+        seo: params.seo,
+      });
     });
   }
 
   onUpdate() {
-    if (this.project.title === '' || this.project.seo === '' || this.project.institution === '' || this.project.shortd === '' || this.project.longd === '' || this.project.contact === '' || this.project.categoryid === '' || this.project.donation === 0|| this.project.goal === 0 || this.project.balance ===0|| this.project.pictureurl===''||this.project.link==='') {
+    if (this.project.title === '' || this.project.seo === '' || this.project.institution === '' || this.project.shortd === '' || this.project.longd === '' || this.project.contact === '' || this.project.categoryid === '' || this.project.donation === 0 || this.project.goal === 0 || this.project.balance === 0 || this.project.pictureurl === '' || this.project.link === '') {
       alert('Please write something to every inputbox')
     } else {
-    this.ds.updateRecordByQuery('projects', { 'seo': this.project.seo }, this.project).subscribe(
-      () => this.router.navigate(["/api/projects"])
-    )
+      this.ds.updateRecordByQuery('projects', { 'seo': this.project.seo }, this.project).subscribe(
+        () => this.router.navigate(["/api/projects"])
+      )
     }
   }
   onKey(event: any) {
