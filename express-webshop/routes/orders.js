385d98d12e0bd10cf2d5c6419e06ec94166f5762
Szilvia Kapuvári <kapuvari.szilvia@gmail.com>
2019-10-01 09:56:40 +0200

    post to orders OK

--- express-webshop/routes/orders.js
+++ express-webshop/routes/orders.js
@@ -6,22 +6,33 @@ const database = new MariaDBmain();
 
 // post a basket oldalról
 router.post('/:projectid', async (req, res) => {
-  if (req.user.id) {
-    const basketItemsWithNamesAndPrices = await database.namingAndPricingProjects(req);
 
-    while (basketItemsWithNamesAndPrices.length) {
-      const basketItem = basketItemsWithNamesAndPrices.shift();
+  if (req.user.id) {
+    //const basketItemsWithNamesAndPrices = await database.namingAndPricingProjects(req);
+    let data = await database.readRecord('baskets', {
+      userid: req.user.id,
+      from: 'INNER JOIN projects ON projects.id = baskets.projectid',
+      select: 'projects.title, projects.donation, projects.id as pid, baskets.quantity as quantity, baskets.id, baskets.projectid, baskets.userid',
+      groupBy: 'projects.id'
+    });
+
+    while (data.length) {
+      const basketItem = data.shift();
+      const ordersQuantity = basketItem.quantity;
+
       await database.createRecord('orders', {
         projectid: basketItem.projectid,
         userid: req.user.id || 0,
-        quantity: 1,
+        quantity: ordersQuantity,
         status: 1
       });
-    }
 
+    }
     await database.deleteRecord('baskets', { userid: req.user.id });
     res.redirect('/baskets');
+
   }
+
 });
 
 module.exports = router;
