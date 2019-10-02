44e33a504f09898ee2dbcaf82f05f904f2adb23e
Pál Sára <pal.sara.ajandek@gmail.com>
2019-10-02 10:47:32 +0200

    Feat:contact ready

--- express-webshop/routes/contact.js
+++ express-webshop/routes/contact.js
@@ -1,38 +1,31 @@
-let express = require('express');
-const Mariadb = require('../modules/webshop-mariadb')
+const express = require('express');
+const dateFormat = require('dateformat');
+const Mariadb = require('../modules/webshop-mariadb');
+
 const database = new Mariadb();
-let router = express.Router();
+const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  const reviewList = await database.readRecord('reviews', {});
-  let newReview = {
-    text: '',
-    rate: '',
-    from: ''
-  };
- function onClick(rating){
-    this.rating = rating;
-    this.ratingClick.emit({
-      itemId: this.itemId,
-      rating: rating
-    });
-  }
+  const reviewList = await database.readRecord('reviews', {
+    from: 'INNER JOIN users ON reviews.userid=users.id',
+    select: 'reviews.text, reviews.rate, users.firstname as firstname, users.lastname as lastname',
+  });
 
   res.render('contact', {
     title: 'Contacts',
     reviews: reviewList,
     user: req.user || {},
+
   });
 });
-router.post('/:id', async (req, res) => {
-  await database.createRecord('baskets', {
-    projectid: req.params.id,
+router.post('/reviews/addReview', async (req, res) => {
+  console.log(req.body.text);
+  await database.createRecord('reviews', {
+    text: req.body.text,
+    rate: req.body.rate,
     userid: req.user.id || 0,
-    quantity: 1,
   });
-  res.redirect('/baskets');
+  res.redirect('/contact');
 });
-router.post('/reviews/addReview', async (req,res,next)=>{
-await database.createRecord('reviews',{})
-})
+
 module.exports = router;
