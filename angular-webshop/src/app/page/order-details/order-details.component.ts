11973e8db2fbf6f3061856a23379294a26795b03
Oláh Dániel <olah.daniel95@gmail.com>
2019-10-03 13:37:08 +0200

    Feat: order-details works

--- angular-webshop/src/app/page/order-details/order-details.component.ts
+++ angular-webshop/src/app/page/order-details/order-details.component.ts
@@ -21,16 +21,22 @@ export class OrderDetailsComponent implements OnInit {
   ngOnInit() {
     let orderID;
     this.ar.params.forEach(param => orderID = param.id);
-    console.log(orderID);
     this.ds.readTableByQuery('orderdetails', {
-      select: 'orderdetails.id as id, orderdetails.insdate as insdate, orderdetails.projectid as projectid, orderdetails.quantity as quantity, orderdetails.status as status, projects.donation as price, projects.title as title',
+      select: 'orderdetails.id as id, orderdetails.insdate as insdate, orderdetails.projectid as projectid, orderdetails.quantity as quantity, orderdetails.status as status, projects.donation as price, projects.title as title, orderdetails.orderid as orderid',
       from: 'INNER JOIN projects ON orderdetails.projectid = projects.id',
       'orderdetails.orderid': orderID,
     });
   }
 
-  log() {
-    console.log(this.orderDetails$);
+  deleteDonation(ev, detailID: number, status: number) {
+    ev.preventDefault();
+    if (!status) {
+      return;
+    }
+    this.ds.deleteRecordByQuery('orderdetails', {
+      id: detailID,
+    });
+    this.router.navigateByUrl('/orders');
   }
 
 }
