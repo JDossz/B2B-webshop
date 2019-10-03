8b331ebfaa75ee6671e9b459ed9e376eedfb4fdd
Juhasz Dorottya <juhasz.dossz@gmail.com>
2019-10-03 11:56:34 +0200

    Feat: profile page only for signed in users

--- express-webshop/routes/myProfile.js
+++ express-webshop/routes/myProfile.js
@@ -4,12 +4,21 @@ const BetagDB = require('./../modules/webshop-mariadb');
 const router = express.Router();
 const database = new BetagDB();
 
-/* GET kérés küldésekor profile page-re: */
-router.get('/', async (req, res, next) => {
-  return res.render('myProfile', {
-    title: 'Profile Page',
-    user: req.user || {},
-  });
+router.all('/', async (req, res, next) => {
+  const auth = await database.checkLogin(req);
+  if (!auth) {
+    return res.render('error-page', {
+      title: 'Nem-nem :)',
+    });
+  } if (auth) {
+    next();
+  }
 });
+
+/* GET kérés küldésekor profile page-re, ha be vagyunk jelentkezve: */
+router.get('/', async (req, res, next) => res.render('myProfile', {
+  title: 'Profile Page',
+  user: req.user || {},
+}));
 
 module.exports = router;
