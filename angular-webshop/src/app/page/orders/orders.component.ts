10bd69d0a290a8d05f6c19f1a1bb019835f32835
Oláh Dániel <olah.daniel95@gmail.com>
2019-10-04 15:05:45 +0200

    Feat: Finished admin side order filtering and sorting

--- angular-webshop/src/app/page/orders/orders.component.ts
+++ angular-webshop/src/app/page/orders/orders.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { ActivatedRoute, Router } from '@angular/router';
-import { DataService } from 'src/app/services/data.service';
+import { Component, OnInit } from "@angular/core";
+import { Observable, BehaviorSubject } from "rxjs";
+import { ActivatedRoute, Router } from "@angular/router";
+import { DataService } from "src/app/services/data.service";
 
 @Component({
-  selector: 'app-orders',
-  templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  selector: "app-orders",
+  templateUrl: "./orders.component.html",
+  styleUrls: ["./orders.component.css"]
 })
 export class OrdersComponent implements OnInit {
+  list$: BehaviorSubject<any> = this.ds.orderList;
+  activeOnly: boolean;
 
-  list$: BehaviorSubject<any> = this.ds.orderList
-
-  constructor (private ds: DataService) { }
+  constructor(private ds: DataService) {}
 
   onDelete(id: number): void {
-    this.ds.deleteRecordByQuery('orders', { 'id': id })
+    this.ds.deleteRecordByQuery("orders", { id: id });
   }
 
   ngOnInit() {
-    this.ds.readTableByQuery('orders', {
-      select: 'users.id as userid, users.firstname, users.lastname, users.address, orders.insdate, orders.quantity, orders.id as orderid, orders.status',
-      from: 'INNER JOIN users ON orders.userid = users.id'
+    this.ds.readTableByQuery("orders", {
+      select:
+        "users.id as userid, users.firstname, users.lastname, users.address, orders.insdate, orders.quantity, orders.id as orderid, orders.status",
+      from: "INNER JOIN users ON orders.userid = users.id"
     });
   }
-
 }
