b081b85d668119bc82f6c7c355dffe31255aac11
Juhasz Dorottya <juhasz.dossz@gmail.com>
2019-10-04 16:15:27 +0200

    Update: new build - error for admin doesnt work properly

--- angular-webshop/src/app/app-routing.module.ts
+++ angular-webshop/src/app/app-routing.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { AppComponent } from "./app.component";
 import { BasketComponent } from "./page/basket/basket.component";
+import { CategoryComponent } from "./page/category/category.component";
+import { CategoryAddComponent } from "./page/category/category-add/category-add.component";
+import { CategoryEditComponent } from "./page/category/category-edit/category-edit.component";
 import { CreateUsersComponent } from "./page/create-users/create-users.component";
 import { IndexComponent } from "./page/index/index.component";
 import { OrdersComponent } from "./page/orders/orders.component";
+import { OrderDetailsComponent } from "./page/order-details/order-details.component";
+import { ProjectsComponent } from "./projects/projects.component";
 import { ProjectAddComponent } from "./project-add/project-add.component";
 import { ProjectEditComponent } from "./project-edit/project-edit.component";
-import { ProjectsComponent } from "./projects/projects.component";
-import { CategoryComponent } from "./page/category/category.component";
-import { CategoryEditComponent } from "./page/category/category-edit/category-edit.component";
-import { CategoryAddComponent } from "./page/category/category-add/category-add.component";
 import { StatisticsComponent } from "./page/statistics/statistics.component";
 import { UpdateOrdersComponent } from "./page/update-orders/update-orders.component";
 import { UpdateUsersComponent } from "./page/update-users/update-users.component";
 import { UsersComponent } from "./page/users/users.component";
-import { OrderDetailsComponent } from "./page/order-details/order-details.component";
 
 const routes: Routes = [
   { path: "", component: StatisticsComponent },
@@ -32,8 +31,7 @@ const routes: Routes = [
   { path: "orders/:id", component: UpdateOrdersComponent },
   { path: "orders/details/:id", component: OrderDetailsComponent },
   { path: "users", component: UsersComponent },
-  { path: "users/:id", component: UpdateUsersComponent },
-  { path: "**", component: StatisticsComponent }
+  { path: "users/:id", component: UpdateUsersComponent }
 ];
 
 @NgModule({
